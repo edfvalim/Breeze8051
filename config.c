de46156ba0f55dffe6635ba96939104df170f804
João Pedro M <joaopedromeloroberto@gmail.com>
2023-06-18 23:42:58 -0400

    config.c modificado

--- config.c
+++ config.c
@@ -7,49 +7,45 @@
 
 // Peripheral specific initialization functions,
 // Called from the Init_Device() function
-void Reset_Sources_Init()
-{
-    WDTCN     = 0xDE;
-    WDTCN     = 0xAD;
-}
-
 void Timer_Init()
 {
     SFRPAGE   = TIMER01_PAGE;
-    TCON      = 0x05;
-    TMOD      = 0x11;
-    CKCON     = 0x18;
-    SFRPAGE   = TMR2_PAGE;
-    TMR2CF    = 0x0A;
-    RCAP2L    = 0x3C;
-    RCAP2H    = 0xF6;
+    TMOD      = 0x01;
+    CKCON     = 0x08;
+    SFRPAGE   = TMR4_PAGE;
+    TMR4CN    = 0x04;
+    TMR4CF    = 0x02;
+    RCAP4L    = 0x8D;
+    RCAP4H    = 0x34;
 }
 
-void ADC_Init()
+void PCA_Init()
 {
-    SFRPAGE   = ADC0_PAGE;
-    ADC0CN    = 0x80;
+    SFRPAGE   = PCA0_PAGE;
+    PCA0CN    = 0x40;
+    PCA0CPM0  = 0x42;
 }
 
-void DAC_Init()
+void UART_Init()
 {
-    SFRPAGE   = DAC0_PAGE;
-    DAC0CN    = 0x9C;
+    SFRPAGE   = UART0_PAGE;
+    SCON0     = 0x70;
 }
 
-void Voltage_Reference_Init()
+void SMBus_Init()
 {
-    SFRPAGE   = ADC0_PAGE;
-    REF0CN    = 0x03;
+    SFRPAGE   = SMB0_PAGE;
+    SMB0CN    = 0x41;
+    SMB0CR    = 0xE9;
 }
 
 void Port_IO_Init()
 {
-    // P0.0  -  Unassigned,  Open-Drain, Digital
-    // P0.1  -  Unassigned,  Open-Drain, Digital
-    // P0.2  -  Unassigned,  Open-Drain, Digital
-    // P0.3  -  Unassigned,  Open-Drain, Digital
-    // P0.4  -  Unassigned,  Open-Drain, Digital
+    // P0.0  -  TX0 (UART0), Open-Drain, Digital
+    // P0.1  -  RX0 (UART0), Open-Drain, Digital
+    // P0.2  -  SDA (SMBus), Open-Drain, Digital
+    // P0.3  -  SCL (SMBus), Open-Drain, Digital
+    // P0.4  -  CEX0 (PCA),  Push-Pull,  Digital
     // P0.5  -  Unassigned,  Open-Drain, Digital
     // P0.6  -  Unassigned,  Open-Drain, Digital
     // P0.7  -  Unassigned,  Open-Drain, Digital
@@ -67,7 +63,7 @@ void Port_IO_Init()
     // P2.1  -  Unassigned,  Push-Pull,  Digital
     // P2.2  -  Unassigned,  Push-Pull,  Digital
     // P2.3  -  Unassigned,  Push-Pull,  Digital
-    // P2.4  -  Unassigned,  Push-Pull,  Digital
+    // P2.4  -  Unassigned,  Open-Drain, Digital
     // P2.5  -  Unassigned,  Open-Drain, Digital
     // P2.6  -  Unassigned,  Open-Drain, Digital
     // P2.7  -  Unassigned,  Open-Drain, Digital
@@ -82,32 +78,19 @@ void Port_IO_Init()
     // P3.7  -  Unassigned,  Open-Drain, Digital
 
     SFRPAGE   = CONFIG_PAGE;
-    P2MDOUT   = 0x1F;
-    P5MDOUT   = 0xFF;
-    P6MDOUT   = 0xFF;
-    P7MDOUT   = 0xFF;
+    P0MDOUT   = 0x10;
+    P2MDOUT   = 0x0F;
+    XBR0      = 0x0D;
     XBR2      = 0x40;
-}
-
-void Oscillator_Init()
-{
-    int i = 0;
-    SFRPAGE   = CONFIG_PAGE;
-    OSCXCN    = 0x67;
-    for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
-    while ((OSCXCN & 0x80) == 0);
-    CLKSEL    = 0x01;
 }
 
 // Initialization function for device,
 // Call Init_Device() from your main program
 void Init_Device(void)
 {
-    Reset_Sources_Init();
     Timer_Init();
-    ADC_Init();
-    DAC_Init();
-    Voltage_Reference_Init();
+    PCA_Init();
+    UART_Init();
+    SMBus_Init();
     Port_IO_Init();
-    Oscillator_Init();
 }
