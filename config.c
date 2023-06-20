905c0c6d9268c182dd1e661b48a1b069759abe49
Eduardo Valim <edvalim@icloud.com>
2023-06-19 21:08:17 -0400

    Configuração correta do confg wizard

--- config.c
+++ config.c
@@ -7,11 +7,19 @@
 
 // Peripheral specific initialization functions,
 // Called from the Init_Device() function
+void Reset_Sources_Init()
+{
+    WDTCN     = 0xDE;
+    WDTCN     = 0xAD;
+}
+
 void Timer_Init()
 {
     SFRPAGE   = TIMER01_PAGE;
-    TMOD      = 0x01;
-    CKCON     = 0x08;
+    TCON      = 0x40;
+    TMOD      = 0x20;
+    CKCON     = 0x18;
+    TH1       = 0xAF;
     SFRPAGE   = TMR4_PAGE;
     TMR4CN    = 0x04;
     TMR4CF    = 0x02;
@@ -23,13 +31,14 @@ void PCA_Init()
 {
     SFRPAGE   = PCA0_PAGE;
     PCA0CN    = 0x40;
+    PCA0MD    = 0x02;
     PCA0CPM0  = 0x42;
 }
 
 void UART_Init()
 {
     SFRPAGE   = UART0_PAGE;
-    SCON0     = 0x70;
+    SCON0     = 0x60;
 }
 
 void SMBus_Init()
@@ -39,20 +48,28 @@ void SMBus_Init()
     SMB0CR    = 0xE9;
 }
 
+void SPI_Init()
+{
+    SFRPAGE   = SPI0_PAGE;
+    SPI0CFG   = 0x40;
+    SPI0CN    = 0x05;
+    SPI0CKR   = 0x7C;
+}
+
 void Port_IO_Init()
 {
-    // P0.0  -  TX0 (UART0), Open-Drain, Digital
+    // P0.0  -  TX0 (UART0), Push-Pull,  Digital
     // P0.1  -  RX0 (UART0), Open-Drain, Digital
-    // P0.2  -  SDA (SMBus), Open-Drain, Digital
-    // P0.3  -  SCL (SMBus), Open-Drain, Digital
-    // P0.4  -  CEX0 (PCA),  Push-Pull,  Digital
-    // P0.5  -  Unassigned,  Open-Drain, Digital
-    // P0.6  -  Unassigned,  Open-Drain, Digital
-    // P0.7  -  Unassigned,  Open-Drain, Digital
+    // P0.2  -  SCK  (SPI0), Push-Pull,  Digital
+    // P0.3  -  MISO (SPI0), Open-Drain, Digital
+    // P0.4  -  MOSI (SPI0), Open-Drain, Digital
+    // P0.5  -  NSS  (SPI0), Push-Pull,  Digital
+    // P0.6  -  SDA (SMBus), Push-Pull,  Digital
+    // P0.7  -  SCL (SMBus), Open-Drain, Digital
 
-    // P1.0  -  Unassigned,  Open-Drain, Digital
-    // P1.1  -  Unassigned,  Open-Drain, Digital
-    // P1.2  -  Unassigned,  Open-Drain, Digital
+    // P1.0  -  CEX0 (PCA),  Push-Pull,  Digital
+    // P1.1  -  T0 (Timer0), Open-Drain, Digital
+    // P1.2  -  T4 (Timer4), Open-Drain, Digital
     // P1.3  -  Unassigned,  Open-Drain, Digital
     // P1.4  -  Unassigned,  Open-Drain, Digital
     // P1.5  -  Unassigned,  Open-Drain, Digital
@@ -63,34 +80,50 @@ void Port_IO_Init()
     // P2.1  -  Unassigned,  Push-Pull,  Digital
     // P2.2  -  Unassigned,  Push-Pull,  Digital
     // P2.3  -  Unassigned,  Push-Pull,  Digital
-    // P2.4  -  Unassigned,  Open-Drain, Digital
+    // P2.4  -  Unassigned,  Push-Pull,  Digital
     // P2.5  -  Unassigned,  Open-Drain, Digital
-    // P2.6  -  Unassigned,  Open-Drain, Digital
+    // P2.6  -  Unassigned,  Push-Pull,  Digital
     // P2.7  -  Unassigned,  Open-Drain, Digital
 
-    // P3.0  -  Unassigned,  Open-Drain, Digital
+    // P3.0  -  Unassigned,  Push-Pull,  Digital
     // P3.1  -  Unassigned,  Open-Drain, Digital
     // P3.2  -  Unassigned,  Open-Drain, Digital
-    // P3.3  -  Unassigned,  Open-Drain, Digital
+    // P3.3  -  Unassigned,  Push-Pull,  Digital
     // P3.4  -  Unassigned,  Open-Drain, Digital
     // P3.5  -  Unassigned,  Open-Drain, Digital
-    // P3.6  -  Unassigned,  Open-Drain, Digital
-    // P3.7  -  Unassigned,  Open-Drain, Digital
+    // P3.6  -  Unassigned,  Push-Pull,  Digital
+    // P3.7  -  Unassigned,  Push-Pull,  Digital
 
     SFRPAGE   = CONFIG_PAGE;
-    P0MDOUT   = 0x10;
-    P2MDOUT   = 0x0F;
-    XBR0      = 0x0D;
-    XBR2      = 0x40;
+    P0MDOUT   = 0x65;
+    P1MDOUT   = 0x01;
+    P2MDOUT   = 0x5F;
+    P3MDOUT   = 0xC9;
+    XBR0      = 0x0F;
+    XBR1      = 0x02;
+    XBR2      = 0x48;
+}
+
+void Oscillator_Init()
+{
+    int i = 0;
+    SFRPAGE   = CONFIG_PAGE;
+    OSCXCN    = 0x67;
+    for (i = 0; i < 3000; i++);  // Wait 1ms for initialization
+    while ((OSCXCN & 0x80) == 0);
+    CLKSEL    = 0x01;
 }
 
 // Initialization function for device,
 // Call Init_Device() from your main program
 void Init_Device(void)
 {
+    Reset_Sources_Init();
     Timer_Init();
     PCA_Init();
     UART_Init();
     SMBus_Init();
+    SPI_Init();
     Port_IO_Init();
+    Oscillator_Init();
 }
